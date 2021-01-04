9a8caed7a08746d97a274b21a59f539f1e0db814
Jon Barson <jbarson@gmail.com>
2021-01-04 11:01:38 -0500

    Cookies ftw

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Cookie from 'js-cookie'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends React.Component {
+  componentDidMount() {
+    Cookie.set("foo", "bar")
+    console.log(Cookie.getJSON("foo"))
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
