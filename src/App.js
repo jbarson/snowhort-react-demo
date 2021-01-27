45450cfcf8d1857f248aba02b5bc236975a4a1ac
brainstation-oll <jbarson@brainstation.io>
2021-01-27 12:32:20 -0500

    hooks demo complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UseStateComponent initialCount={9}/>
     </div>
   );
 }
 
 export default App;
+
+const UseStateComponent = ({initialCount }) => { 
+  const [count, setCount] = useState(initialCount)
+  const [anotherCount, setAnotherCount] = useState(0)
+  const handleClick = () => setCount(prevCount => prevCount + 1)
+  const anotherClick = () => setAnotherCount(anotherCount +10)
+  useEffect(() => {
+    document.title = `Count ${count}`
+  }, [count])
+  useEffect(() => {
+    console.log(`Count ${anotherCount}`)
+  },[anotherCount])
+  return (
+    <div>
+      <p>{initialCount}</p>
+      
+      <h1>The count is {count}</h1>
+      <h1>Another count is {anotherCount}</h1>
+      <button onClick={handleClick}>click me!</button>
+      <button onClick={anotherClick}>click me too!</button>
+    </div>
+  )
+}
